7297b67108f481a8936f4f8eac42e5d6c6d7a395
sanazbeauty <sani.m7063@gmail.com>
2020-08-12 09:44:15 +0430

    Variable is JS

---
+++ javascript/js.js
@@ -0,0 +1,17 @@
+
+
+
+// var firstName = "sanaz"
+// var lastName = "beauty"
+// var age = 30
+
+// console.log("my name is" + " " + firstName + " " + lastName + " " + age)
+// console.log(`My name is ${firstName} ${lastName} and im ${age} Years Old`)
+
+var grade = 20
+
+if (grade <= 10) {
+    console.log(`Your grade is ${grade} you failed`)
+} else if (grade > 10) {
+    console.log(`Your grade is ${grade} you Passed`)
+}
